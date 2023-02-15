3bd3003ef347db852f7ab107cf75e392e5956150
Andrew Dunn <adunn@silverstripe.com>
2023-02-16 11:38:48 +1300

    Fix php linting and tidy up

--- src/Traits/CertificateAdminPermissionsTrait.php
+++ src/Traits/CertificateAdminPermissionsTrait.php
@@ -2,7 +2,6 @@
 
 namespace SilverStripe\SmimeForms\Traits;
 
-use SilverStripe\Security\Member;
 use SilverStripe\Security\Permission;
 use SilverStripe\SmimeForms\Admin\EncryptionAdmin;
 
@@ -11,24 +10,34 @@ trait CertificateAdminPermissionsTrait
 
     /**
      * Permissions for viewing certificates.
+     *
+     * @param null $member
+     * @return bool
      */
-    public function canView(?Member $member = null): bool
+    public function canView($member = null): bool
     {
         return Permission::check(EncryptionAdmin::PERMISSION_SMIME_ENCRYPTION_ADMIN);
     }
 
     /**
      * Permissions for editing certificates.
+     *
+     * @param null $member
+     * @return bool
      */
-    public function canEdit(?Member $member = null): bool
+    public function canEdit($member = null): bool
     {
         return Permission::check(EncryptionAdmin::PERMISSION_SMIME_ENCRYPTION_ADMIN);
     }
 
     /**
      * Permissions for creating certificates.
+     *
+     * @param null $member
+     * @param array $context
+     * @return bool
      */
-    public function canCreate(?Member $member = null, array $context = []): bool
+    public function canCreate($member = null, array $context = []): bool
     {
         return Permission::check(EncryptionAdmin::PERMISSION_SMIME_ENCRYPTION_ADMIN);
     }
